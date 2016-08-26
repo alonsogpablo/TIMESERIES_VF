554c54df02c1d15fe03fafd9ea89bf0f81e7cf4b
Pablo <alonsogpablo@gmail.com>
2016-08-26 13:39:03 +0200

    Initial commit

---
+++ ANALIZA_DT.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+
+
+
+ts=pd.read_csv('/Users/Pablo/Desktop/DT_3G_ERI.csv',parse_dates=[0])
+
+data= ts.pivot(index='fecha', columns='nodo', values='sum_dt')
+
+
+data.fillna(0,inplace=True)
+
+for column in data:
+    if data[column].mean()==0: del data[column]
+
+
+for i,row in data.mean().iteritems():
+    print i,data[i].mean()
